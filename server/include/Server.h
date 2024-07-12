b52234f476ed5e250c70cde52506f76797ebbfa4
Семён Сайидов <timefrid@yandex.ru>
2024-07-12 09:02:47 +0300

    Изменение области видимости функции shutdown() в классе сервера

--- server/include/Server.h
+++ server/include/Server.h
@@ -6,11 +6,11 @@ private:
 	NetworkModule network_module;
 	Storage storage;
 	PackageManager package_manager;
+	void shutdown(); //закрывает сокет
 public:
 	Server(uint32_t server_ip, uint16_t port); //запуск сервера, бинд к порту
 	pollfd* toPoll(); //запускает network_module.toPoll() и возвращает список готовых ивентов
 	uint8_t eventHandler(pollfd fd); //делает reevent=0, обрабатывает событие, взаимодействует с PackageManager и Storage, возвращает статус
-	void shutdown(); //закрывает сокет
 };
 
 #endif
