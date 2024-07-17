4710452319ddde1b047c7dc9c11b0b9af5ccc331
Семён Сайидов <timefrid@yandex.ru>
2024-07-17 17:02:29 +0300

    Доработка пакетного менеджера сервера

--- server/src/PackageManager.cpp
+++ server/src/PackageManager.cpp
@@ -12,7 +12,7 @@ uint8_t PackageManager::readHeaderFields(const char* buffer, uint16_t* cmd, uint
 
 uint8_t PackageManager::parseToPackage(Package* package, const char* buffer) { //парсит присланный буфер в структуру
     uint16_t cmd, payload;
-    uint8_t status = readCommand(buffer, &cmd, &payload);
+    uint8_t status = readHeaderFields(buffer, &cmd, &payload);
     if (cmd > CMD_MAX || payload > PAYLOAD_MAX) {
         return E_DATA;
     }
@@ -39,31 +39,30 @@ uint8_t PackageManager::transferToBuffer(Package package, char* buffer) { //фо
 }
 
 void PackageManager::createErrorPackage(Package* package, uint8_t error_code) { //формирует пакет ERROR_MSG
-    package.header = { ERROR_CODE, sizeof(package.data.s_error_code) };
-    package.data.s_error_code.error_code = error_code;
+    package->header = { ERROR_MSG, sizeof(package->data.s_error_msg) };
+    package->data.s_error_msg.error_code = error_code;
     return;
 }
 
 void PackageManager::createAuthConfirmPackage(Package* package, uint32_t client_uid) { //формирует пакет AUTH_CONFIRM
-    package.header = { AUTH_CONFIRM, sizeof(package.data.s_auth_confirm) };
-    package.data.s_auth_confirm.client_uid = client_uid;
+    package->header = { AUTH_CONFIRM, sizeof(package->data.s_auth_confirm) };
+    package->data.s_auth_confirm.client_uid = client_uid;
     return;
 }
 
-void PackageManager::createUserListPackage(Package* package, std::map<std::string, uint32_t> clientData) { //формирует пакет USER_LIST
-    package.header = { USER_LIST, (clientData.size() * sizeof(package.data.s_user_list.user[0])) };
-    map<string, uint32_t>::iterator it;
+void PackageManager::createUserListPackage(Package* package, std::map<std::string, uint32_t> u_list) { //формирует пакет USER_LIST
+    package->header = { USER_LIST, (uint16_t)(u_list.size() * sizeof(package->data.s_user_list.user[0])) };
+    std::map<std::string, uint32_t>::iterator it;
     uint8_t j = 0;
-    login_size = sizeof(package.data.user[j].login);
-    for (it = clientData.begin(); it != clientData.end(); it++, j++) {
-        strncpy(package.data.user[j].login, it->first, login_size);
-        package.data.user[j].uid = it->second;
+    unsigned int login_size = sizeof(package->data.s_user_list.user[j].login);
+    for (it = u_list.begin(); it != u_list.end(); it++, j++) {
+        strncpy(package->data.s_user_list.user[j].login, it->first.c_str(), login_size);
+        package->data.s_user_list.user[j].uid = it->second;
     }
-    return;
 }
 
 void PackageManager::createExitFriendPackage(Package package, uint32_t friend_uid) { //формирует пакет EXIT_FRIEND
     package.header = { EXIT_FRIEND, sizeof(package.data.s_exit_friend) };
-    package.data.s_exit_client.friend_uid = friend_uid;
+    package.data.s_exit_friend.friend_uid = friend_uid;
     return;
 }
