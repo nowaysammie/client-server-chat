fc671bfb296e6662c34cec01ed8b3e2b81f8ac03
Семён Сайидов <timefrid@yandex.ru>
2024-07-17 17:02:29 +0300

    Доработка пакетного менеджера сервера

--- server/include/PackageManager.h
+++ server/include/PackageManager.h
@@ -1,6 +1,7 @@
 #ifndef PACKAGE_MANAGER_H
 #define PACKAGE_MANAGER_H
 
+#include <iterator>
 #include <map>
 #include <string>
 #include <cstring>
@@ -8,7 +9,7 @@
 #include "Package.h"
 #include "States.h"
 
-class PackageManager() {
+class PackageManager {
 private:
 	uint8_t readHeaderFields(const char* buffer, uint16_t* cmd, uint16_t* payload); //считывает код команды
 public:
@@ -16,7 +17,7 @@ public:
 	uint8_t transferToBuffer(Package package, char* buffer); //формирует буфер из структуры
 	void createErrorPackage(Package* package, uint8_t error_code); //формирует пакет ERROR_MSG
 	void createAuthConfirmPackage(Package* package, uint32_t client_uid); //формирует пакет AUTH_CONFIRM
-	void createUserListPackage(Package* package, std::map<std::string, uint32_t>); //формирует пакет USER_LIST
+	void createUserListPackage(Package* package, std::map<std::string, uint32_t> u_list); //формирует пакет USER_LIST
 	void createExitFriendPackage(Package package, uint32_t friend_uid); //формирует пакет EXIT_FRIEND
 };
 
