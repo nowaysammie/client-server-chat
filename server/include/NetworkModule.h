b0e9014b54d68598cb96779042a444e3e65f7d3e
Семён Сайидов <timefrid@yandex.ru>
2024-07-18 09:20:46 +0300

    Исправление ошибок в коде

--- server/include/NetworkModule.h
+++ server/include/NetworkModule.h
@@ -21,18 +21,18 @@ private:
 	sockaddr_in serverAddress; // адрес сервера
 	nfds_t count_fds;		   // количество элементов в fds
 	pollfd *fds;			   // массив структур (users + stdin)
-	
+
 public:
-	uint8_t init(uint32_t server_ip, uint16_t port);				// происходит инициализация, создаётся сокет, сокет биндится к порту
-	void toPoll();													// запускает опрос всех сокетов
-	uint8_t appendClient();											// добавляем клиента в отслеживаемый массив !узнать тип event
-	uint8_t removeClient(int32_t client_socket);					// убрать клиента из списка отслеживаемых
-	uint8_t getMessage(int32_t client_socket, char *buffer);		// получить сообщение
-	uint8_t sendMessage(int32_t client_socket, const char *buffer); // отправить сообщение
-	pollfd *readyFd();												// возвращает указатель на готовый к обработке файловый дескриптор
-	uint8_t getClientSocket(uint32_t client_uid, int32_t* dest_socket); //РЕАЛИЗОВАТЬ!!!
-	pollfd *getFd(int index); //возвращает указатель на элемент fds[index]
-	void closeSocket();												// закрыть сокет сервера
+	uint8_t init(uint32_t server_ip, uint16_t port);					// происходит инициализация, создаётся сокет, сокет биндится к порту
+	void toPoll();														// запускает опрос всех сокетов
+	uint8_t appendClient();												// добавляем клиента в отслеживаемый массив !узнать тип event
+	uint8_t removeClient(int32_t client_socket);						// убрать клиента из списка отслеживаемых
+	uint8_t getMessage(int32_t client_socket, char *buffer);			// получить сообщение
+	uint8_t sendMessage(int32_t client_socket, const char *buffer);		// отправить сообщение
+	pollfd *readyFd();													// возвращает указатель на готовый к обработке файловый дескриптор
+	uint8_t getClientSocket(uint32_t client_uid, int32_t *dest_socket); // РЕАЛИЗОВАТЬ!!!
+	pollfd *getFd(unsigned int index);									// возвращает указатель на элемент fds[index]
+	void closeSocket();													// закрыть сокет сервера
 };
 
 #endif
