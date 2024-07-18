cbb8d10b2ebe76560efa8ba986725ca139b598ba
Семён Сайидов <timefrid@yandex.ru>
2024-07-18 10:42:41 +0300

    Исправление ошибок в NetworkModule

--- client/src/NetworkModule.cpp
+++ client/src/NetworkModule.cpp
@@ -1,62 +1,75 @@
-#include "NetworkModule.h"
+#include "../include/NetworkModule.h"
 
-//клиентский NetworkModule
+// клиентский NetworkModule
 
-uint8_t NetworkModule::init(uint32_t server_ip, uint16_t port) {
-	//создание массива файловых дескрипторов, добавление ФД потока ввода в массив опрашиваемых ФД 
-	count_fds = 2;
+uint8_t NetworkModule::init(char *server_ip, uint16_t port)
+{
+	// создание массива файловых дескрипторов, добавление ФД потока ввода в массив опрашиваемых ФД
 	fds = new pollfd[2];
 	fds[0].fd = fileno(stdin);
 	fds[0].events = POLLIN;
-	//настройка протокола, ip-адреса и порта
+	// настройка протокола, ip-адреса и порта
 	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_addr.s_addr = server_ip; //inet_pton и тд.
-	serverAddress.sin_port = htons(port); //работает?
-	//создание сокета
-	socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket < 0) {
+	int state = inet_pton(AF_INET, server_ip, &serverAddress.sin_addr.s_addr);
+	if (state <= 0)
+	{
 		return E_CONNECT;
 	}
-	//разблокировка сокета
-	fcntl(socket, F_SETFL, O_NONBLOCK);
+	serverAddress.sin_port = htons(port); // работает?
+	// создание сокета
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket < 0)
+	{
+		return E_CONNECT;
+	}
+	// разблокировка сокета
+	fcntl(clientSocket, F_SETFL, O_NONBLOCK);
 	return SUCCESS;
 }
 
-uint8_t NetworkModule::connectToServer() {
-	//попытка присоединиться к серверу
-	int state = connect(serverAddress, (sockaddr*) &serverAddress, sizeof(serverAddress));
-	if (state < 0) {
+uint8_t NetworkModule::connectToServer()
+{
+	// попытка присоединиться к серверу
+	int state = connect(clientSocket, (sockaddr *)&serverAddress, sizeof(serverAddress));
+	if (state < 0)
+	{
 		return E_CONNECT;
 	}
-	fds[1].fd = socket;
+	fds[1].fd = clientSocket;
 	fds[2].events = POLLIN;
 	return SUCCESS;
 }
 
-void NetworkModule::toPoll() {
-	//опрос сокетов неопределённое время
-	int state = poll(fds, count_fds, -1); 
+void NetworkModule::toPoll()
+{
+	// опрос сокетов неопределённое время
+	int state = poll(fds, 2, -1);
 }
 
-uint8_t NetworkModule::getMessage(char* buffer) {
-	//получаем сообщение от клиента
-	int state = recv(socket, buffer, BUFFER_SIZE, 0);
-	if (state <= 0) {
+uint8_t NetworkModule::getMessage(char *buffer)
+{
+	// получаем сообщение от клиента
+	int state = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+	if (state <= 0)
+	{
 		return E_CONNECT;
 	}
 	return SUCCESS;
 }
 
-uint8_t NetworkModule::sendMessage(const char* buffer) {
-	//отправляем сообщение от клиенту
-	int state = send(socket, buffer, BUFFER_SIZE, 0);
-	if (state <= 0) {
+uint8_t NetworkModule::sendMessage(const char *buffer)
+{
+	// отправляем сообщение от клиенту
+	int state = send(clientSocket, buffer, BUFFER_SIZE, 0);
+	if (state <= 0)
+	{
 		return E_CONNECT;
 	}
 	return SUCCESS;
 }
 
-void NetworkModule::closeSocket() {
-	//закрытие сокета клиента
-	close(socket);
+void NetworkModule::closeSocket()
+{
+	// закрытие сокета клиента
+	close(clientSocket);
 }
