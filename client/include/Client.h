6ba8e8cf83fd802bd1598ddfd44a70f28c7ee65a
Семён Сайидов <timefrid@yandex.ru>
2024-07-18 10:48:34 +0300

    Редактирование Client

--- client/include/Client.h
+++ client/include/Client.h
@@ -1,22 +1,28 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-class Client {
+#include "NetworkModule.h"
+#include "ClientStorage.h"
+#include "PackageManager.h"
+#include "UI.h"
+
+class Client
+{
 private:
 	NetworkModule network_module;
 	ClientStorage client_storage;
 	PackageManager package_manager;
 	UI ui;
-	pollfd* actions; //стандартный поток ввода и серверный сокет
-	void disconnect(); //закрывает сокет
-	uint8_t getMessage(char* buffer); //получить сообщение
-	uint8_t sendMessage(struct Package); //отправить сообщение
+	pollfd *actions;					 // стандартный поток ввода и серверный сокет
+	void disconnect();					 // закрывает сокет
+	uint8_t getMessage(char *buffer);	 // получить сообщение
+	uint8_t sendMessage(struct Package); // отправить сообщение
 public:
-	Client(uint32_t server_ip, uint16_t port); //происходит инициализация, создаётся сокет
-	uint8_t connectToServer(); //попытка присоединиться к серверу
-	pollfd* toPoll(); //отслеживает стандартный поток ввода и серверный сокет
-	void printStartScreen(char* login); //вывод Welcome и запрос логина
-	uint8_t eventHandler(pollfd fd); //обрабатывает либо данные с потока ввода, либо присланные данные, взаимодействует с PackageManager, ClientStorage и UI
+	uint8_t init(char *server_ip, uint16_t port); // происходит инициализация, создаётся сокет
+	uint8_t connectToServer();					  // попытка присоединиться к серверу
+	pollfd *toPoll();							  // отслеживает стандартный поток ввода и серверный сокет
+	void printStartScreen(char *login);			  // вывод Welcome и запрос логина
+	uint8_t eventHandler(pollfd fd);			  // обрабатывает либо данные с потока ввода, либо присланные данные, взаимодействует с PackageManager, ClientStorage и UI
 };
 
 #endif
--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -1,33 +1,27 @@
-#include"Client.h"
+#include "Client.h"
 
 void Client::disconnect()
 {
-	network_module.clientSocket();
+	network_module.closeSocket();
 }
-
 
 uint8_t Client::eventHandler(pollfd fd)
 {
-
 }
 
-
-void Client::printStartScreen(char* login)
+void Client::printStartScreen(char *login)
 {
 
 	ui.printWelcome();
-	ui.askLogin(); 
+	ui.askLogin();
 }
 
-
-pollfd* Client::toPoll()
+pollfd *Client::toPoll()
 {
 	network_module.toPoll();
 }
 
-
-uint8_t Client::init(uint32_t server_ip, uint16_t port)
+uint8_t Client::init(char *server_ip, uint16_t port)
 {
 	return network_module.init(server_ip, port);
-
 }
